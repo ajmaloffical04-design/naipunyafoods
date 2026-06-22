eeaa3bd4f081a65602e30adfe65ae0138f16fe5a
ajmaloffical04-design <ajmaloffical04@gmail.com>
2026-06-22 11:08:42 +0530

    Fix GitHub Pages deployment paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = '';
+if (isGithubActions && process.env.GITHUB_REPOSITORY) {
+  repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+}
+
 const nextConfig: NextConfig = {
+  output: isGithubActions ? "export" : undefined,
+  basePath: isGithubActions ? `/${repo}` : "",
   images: {
+    unoptimized: !!isGithubActions,
     formats: ["image/avif", "image/webp"],
     deviceSizes: [375, 640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 64, 96, 128, 256, 384],
